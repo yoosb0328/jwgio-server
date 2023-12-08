e48799368cdf44da2c5dff288e820afcbe91c089
yoosb0328 <yoosb0328@gmail.com>
2023-12-08 16:21:27 +0900

    first

--- src/main/java/com/study/springlambda/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.study.springlambda.repository;
-
-import com.study.springlambda.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
